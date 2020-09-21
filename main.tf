3d953008094e770826b78ba6f9f4720b3096e675
Shamanth <shamanth@DevopsGuy.local>
2020-09-21 11:43:05 +0530

    tf file

---
+++ main.tf
@@ -0,0 +1,144 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAIJRSR2FX6KDUEVEA"
+  secret_key = "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"
+}
+
+# 1. Create a VPC 
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Production"
+  }
+}
+
+# 2. Create a IGW
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.first-vpc.id
+  tags = {
+    Name = "first-igw"
+  }
+}
+
+# 3. Create a Route-table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.first-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-rtb"
+  }
+}
+
+# 4. Create a Subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.first-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Prod-subnet"
+  }
+}
+
+# 5. Route table Association
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security group
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web-traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.first-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Create Network Interface
+
+resource "aws_network_interface" "test" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# 8. Create Eip
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.test.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw, aws_instance.my-first-server]
+}
+
+# 9. Create Ubuntu server and install/enable apache2
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-06b263d6ceff0b3dd"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "main-key"
+
+  network_interface {
+      device_index = 0
+      network_interface_id = aws_network_interface.test.id
+  }
+  user_data = <<-EOF
+       #!/bin/bash
+       sudo apt update -y
+       sudo apt install apache2 -y
+       sudo systemctl start apache2
+       sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+       EOF
+
+  tags = {
+    Name = "Webserver"
+  }
+}
